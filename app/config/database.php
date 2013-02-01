6ccac49e052408e352dbeb26052d93c4490678b4
ChrisBorgia <cborgia@gmail.com>
2013-02-01 12:19:45 -0500

    added redis suport to boxfile

--- app/config/database.php
+++ app/config/database.php
@@ -112,8 +112,8 @@ return array(
 	'redis' => array(
 
 		'default' => array(
-			'host'     => '127.0.0.1',
-			'port'     => 6379,
+			'host'     => $_SERVER["CACHE1_HOST"],
+			'port'     => $_SERVER["CACHE1_PORT"],
 			'database' => 0,
 		),
 
